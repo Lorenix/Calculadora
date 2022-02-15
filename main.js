6995642461a2a6669d9d8593fa22b2404f93cfbe
Lorena <lorena.del88@hotmail.com>
2022-02-15 11:24:57 +0100

    suma y multiplicación

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ console.log("hello");
 
 var numero2 = 2;
 var numero4 = 4;
+var numero3 = 3;
 
 console.log(numero2 + numero4)
-var resultado = 6
+var resultado = numero2 + numero4
+
 
 console.log(numero4 - numero2)
-var resultado = 2
+var resultado = numero4 - numero2
+
 
 console.log(numero4 * numero2)
+var resultado = numero4 * numero2
 
-
-
+console.log(numero2 * numero3)
+var resultado = numero2 * numero3
 
 
