30975759b7e67cc1b0596d85f32e07252eebcb41
Lorena <lorena.del88@hotmail.com>
2022-02-15 11:13:57 +0100

    variables calculadora

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+var numero2 = 2;
+var numero4 = 4;
+
+console.log(numero2 + numero4)
+var resultado = 6
+
+console.log(numero4 - numero2)
+var resultado = 2
+
+console.log(numero4 * numero2)
+
+
+
+
+
